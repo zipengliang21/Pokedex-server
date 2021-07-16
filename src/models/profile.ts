a98c3df6e5a574fe803ef279486ad03db4b2e228
HongxiG <r3j2b@ugrad.cs.ubc.ca>
2021-07-16 15:45:28 -0700

    minor change

--- src/models/profile.ts
+++ src/models/profile.ts
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 import db from "../mongodb.config";
 
@@ -20,6 +20,12 @@ const AvatarSchema = new instance.Schema({
     contentType: String,
 })
 
+// const CollectionSchema = new instance.Schema({
+//     pokemonId:String;
+// })
+
+// const userCollect = mongoose.model('userCollect', CollectionSchema);
+
 const profileSchema = new instance.Schema({
     userId: {type: String, required: true},
     userName: {type: String, required: true},
@@ -27,6 +33,7 @@ const profileSchema = new instance.Schema({
     password:{type:String,required:true},
     location:{type:String},
     avatar: AvatarSchema,
+    // userCollect: [{ type: Schema.Types.ObjectId, ref: 'userCollect' }]
 }, { collection: "profile"})
 
 export const Profile = instance.model<any>('Profile', profileSchema);
