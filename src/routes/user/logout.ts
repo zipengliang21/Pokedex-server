964381751f21385aa9bb2410fe7b18975e482a70
UBC Student <cjwen14@students.cs.ubc.ca>
2021-07-21 10:33:50 -0700

    modify admin schema

--- src/routes/user/logout.ts
+++ src/routes/user/logout.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 
 export default async (req: Request, res: Response): Promise<void> => {
     res.cookie('jwt', '', {
-        expires: new Date(Date.now() + 10 * 1000),
+        expires: new Date(Date.now() + 1*1000),
         httpOnly: true,
     });
     res.status(200).json({});
