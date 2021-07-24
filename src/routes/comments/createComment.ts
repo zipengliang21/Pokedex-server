dc97fb04ca334c139a20efab4f0e8d23b7753525
UBC Student <cjwen14@students.cs.ubc.ca>
2021-07-24 13:08:45 -0700

    add catch

--- src/routes/comments/createComment.ts
+++ src/routes/comments/createComment.ts
@@ -23,16 +23,16 @@ export default async (req: Request, res: Response): Promise<void> => {
         throw new ServerError({ statusCode: 400, message: MESSAGES.EMPTY_COMMENT });
     }
 
-    let articleExists: boolean;
+    let postExists: boolean;
     try {
-        articleExists = await Posts.exists({ postID: postID });
+        postExists = await Posts.exists({ postID: postID });
     } catch (err) {
         throw new ServerError({
             statusCode: 400,
             message: MESSAGES.POST_ID_NOT_FOUND,
         });
     }
-    if (!articleExists) {
+    if (!postExists) {
         throw new ServerError({
             statusCode: 400,
             message: MESSAGES.POST_ID_NOT_FOUND,
--- src/routes/user/login.ts
+++ src/routes/user/login.ts
@@ -5,47 +5,59 @@ import bcrypt from "bcrypt";
 import {sendToken} from "../../middleware/auth";
 
 export default async (req: Request, res: Response): Promise<void> => {
-    const {
-        email,
-        password
-    } = req.body;
+        const {
+            email,
+            password
+        } = req.body;
+      try {
+          if (!email) {
+              throw new ServerError({
+                  message: "Please provide your email address",
+                  statusCode: 400,
+              });
+              // res.status(400).send({err:"Please provide your email address"});
+          }
 
-    if (!email) {
-        throw new ServerError({
-            message: "Please provide your email address",
-            statusCode: 400,
-        });
-    }
+          if (!password) {
+              throw new ServerError({
+                  message: "Please provide your password",
+                  statusCode: 400,
+              });
+              // res.status(400).send("Please provide your password");
+          }
 
-    if (!password) {
-        throw new ServerError({
-            message: "Please provide your password",
-            statusCode: 400,
-        });
-    }
+          const user: UserDocument = await User.findOne({email});
 
-    const user: UserDocument = await User.findOne({ email });
+          if (!user) {
+              throw new ServerError({
+                  message: "Unable to login, no user with this email",
+                  statusCode: 400,
+              });
+              // res.status(400).send("no such user");
+          }
 
-    if (!user) {
-        throw new ServerError({
-            message: "Unable to login",
-            statusCode: 400,
-        });
-    }
+          const isMatch = await bcrypt.compare(password, user.password)
 
-    const isMatch = await bcrypt.compare(password, user.password)
+          if (!isMatch) {
+              throw new ServerError({
+                  message: "Wrong Password",
+                  statusCode: 400,
+              });
+              // res.status(400).send({err:"password not match"});
+          } else {
+              await sendToken({
+                  origin: req.get('Origin'),
+                  user: user,
+                  statusCode: 200,
+                  res: res,
+              });
+          }
+      }catch (err) {
+          if (err instanceof ServerError) {
+              res.status(err.statusCode).send(err.message);
+          } else {
+              res.status(500).send("Unexpected error");
+          }
+      }
 
-    if (!isMatch) {
-        throw new ServerError({
-            message: "Wrong Password",
-            statusCode: 400,
-        });
-    }
-
-    await sendToken({
-        origin: req.get('Origin'),
-        user: user,
-        statusCode: 200,
-        res: res,
-    });
 }
