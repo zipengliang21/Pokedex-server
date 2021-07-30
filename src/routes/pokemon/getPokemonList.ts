988b9d001cf3df9441e2bf7bf111e46397822354
UBC Student <cjwen14@students.cs.ubc.ca>
2021-07-30 16:52:47 -0700

    fix some bug

--- src/routes/pokemon/getPokemonList.ts
+++ src/routes/pokemon/getPokemonList.ts
@@ -2,7 +2,8 @@ import { Request, Response } from 'express';
 import {Pokemon, PokemonDocument} from "../../models/pokemon";
 
 export default async (req: Request, res: Response): Promise<void> => {
-    const pokemonList: PokemonDocument[] = await Pokemon.find({ });
+    const pokemonList: PokemonDocument[] = await Pokemon.find({ })
+                                                        .sort({id: 1});
 
     res.status(200).json({ pokemonList, count: pokemonList.length });
 };
